cd27e3bdbbfbb6e6855ab2a7c8b495f8eed3c868
Milica Vidovic <milica.vidovic92@gmail.com>
2019-06-18 09:31:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,41 +6,30 @@ const cameraView = document.querySelector("#camera--view"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger")
 
-
-    
-
-      if (window.DeviceOrientationEvent) {
-        window.addEventListener("deviceorientation", function(event) {
-            console.log('data', event)
-        }, true);
+window.addEventListener("deviceorientation", function(event) {
+    if (event.alpha === 0 || event.beta === 90 || event.gamma === 0) {
+        function cameraStart() {
+            navigator.mediaDevices
+                .getUserMedia(constraints)
+                .then(function(stream) {
+                track = stream.getTracks()[0];
+                cameraView.srcObject = stream;
+            })
+            .catch(function(error) {
+                console.error("Oops. Something is broken.", error);
+            });
+        }
+        // Take a picture when cameraTrigger is tapped
+        cameraTrigger.onclick = function() {
+            cameraSensor.width = cameraView.videoWidth;
+            cameraSensor.height = cameraView.videoHeight;
+            cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+            cameraOutput.src = cameraSensor.toDataURL("image/webp");
+            cameraOutput.classList.add("taken");
+        };
+        // Start the video stream when the window loads
+        window.addEventListener("load", cameraStart, false);
+    } else {
+        console.log ('You need to set position of your device')
     }
-    
-    var handleOrientationEvent = function(alpha, beta, gamma) {
-        // do something amazing
-        console.log('data', alpha)
-    };
-
-    
-      
-// Access the device camera and stream to cameraView
-function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
-    });
-}
-// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-};
-// Start the video stream when the window loads
-window.addEventListener("load", cameraStart, false);
+}, true);
