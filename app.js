13e4b03ff08a04bb8bb4e61b605a7242db9e6f3f
Milica Vidovic <milica.vidovic92@gmail.com>
2019-06-18 12:23:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,11 +12,7 @@ const cameraView = document.querySelector("#camera--view"),
      }else {document.getElementById('do-info').classList.remove('hidden');
 
      window.addEventListener('deviceorientation', function(event) {
-        document.getElementById('cube').style.webkitTransform =
-        document.getElementById('cube').style.transform =
-                'rotateX(' + event.beta + 'deg) ' +
-                'rotateY(' + event.gamma + 'deg) ' +
-                'rotateZ(' + event.alpha + 'deg)';
+        
 
         document.getElementById('beta').innerHTML = Math.round(event.beta);
         document.getElementById('gamma').innerHTML = Math.round(event.gamma);
