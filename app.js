c80c385495b5d328cdefe1eb90a9496970a491fb
Milica Vidovic <milica.vidovic92@gmail.com>
2019-06-19 16:27:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -26,13 +26,7 @@ const cameraView = document.querySelector("#camera--view"),
 
   // Because we don't want to have the device upside down
   // We constrain the x value to the range [-90,90]
-  if (x >  90) { x =  90};
-  if (x < -90) { x = -90};
-
-  // To make computation easier we shift the range of 
-  // x and y to [0,180]
-  x += 90;
-  y += 90;
+  
 
   // 10 is half the size of the ball
   // It center the positioning point to the center of the ball
