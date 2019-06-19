68eb27294c77ecb5b3fdb5773d3df4cd23243720
Milica Vidovic <milica.vidovic92@gmail.com>
2019-06-19 16:25:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,18 +5,41 @@ const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger")
-    
+ 
+    var ball   = document.querySelector('.ball');
+    var garden = document.querySelector('.garden');
+    var output = document.querySelector('.output');
+
+    var maxX = garden.clientWidth  - ball.clientWidth;
+    var maxY = garden.clientHeight - ball.clientHeight;
        
         window.addEventListener("deviceorientation", function(e) {
             var z = e.alpha;
-            var y = e.beta;
-            var x = e.gamma;
+            var x = e.beta;
+            var y = e.gamma;
             document.getElementById('beta').innerHTML = Math.round(y);
             document.getElementById('gamma').innerHTML = Math.round(x);
             document.getElementById('alpha').innerHTML = Math.round(z);
             console.log("test", y);
+            output.innerHTML  = "beta : " + x + "\n";
+  output.innerHTML += "gamma: " + y + "\n";
 
-            if ( y < 92 && y > 87) {
+  // Because we don't want to have the device upside down
+  // We constrain the x value to the range [-90,90]
+  if (x >  90) { x =  90};
+  if (x < -90) { x = -90};
+
+  // To make computation easier we shift the range of 
+  // x and y to [0,180]
+  x += 90;
+  y += 90;
+
+  // 10 is half the size of the ball
+  // It center the positioning point to the center of the ball
+  ball.style.top  = (maxX*x/180 - 10) + "px";
+  ball.style.left = (maxY*y/180 - 10) + "px";
+
+            if ( x < 92 && x > 87) {
                 console.log('uspela si')
                 cameraTrigger.onclick = function() {
                     cameraSensor.width = cameraView.videoWidth;
