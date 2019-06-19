0ec551bcd1774197228453652cc4995c6baf0a66
Milica Vidovic <milica.vidovic92@gmail.com>
2019-06-19 14:24:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger")
-    if (!('ondeviceorientation' in window)) {
+    if (DeviceOrientationEvent.window) {
         document.getElementById('do-unsupported').classList.remove('hidden');
      } 
 
