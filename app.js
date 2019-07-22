9ad4b8aad8d7ffa83b21bc0628f30d7b3db10abf
Milica Vidovic <milica.vidovic92@gmail.com>
2019-07-22 18:59:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const cameraView = document.querySelector("#camera--view"),
         cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
         cameraOutput.src = cameraSensor.toDataURL("image/webp");
         cameraOutput.classList.add("taken");
-      }, 2000);
+      }, 5000);
     }
   },
   true
