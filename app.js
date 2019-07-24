6397a19db9e35a26233be7b5ae54126b4904fe16
Milica Vidovic <milica.vidovic92@gmail.com>
2019-07-24 08:56:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 var constraints = { video: { facingMode: "user" }, audio: false };
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")
-    
-       
-        window.addEventListener(
+  cameraOutput = document.querySelector("#camera--output"),
+  cameraSensor = document.querySelector("#camera--sensor"),
+  cameraTrigger = document.querySelector("#camera--trigger");
+
+let y = 0;
+window.addEventListener(
   "deviceorientation",
   function(e) {
     var z = e.alpha;
@@ -17,7 +17,7 @@ const cameraView = document.querySelector("#camera--view"),
     document.getElementById("gamma").innerHTML = Math.round(x);
     document.getElementById("alpha").innerHTML = Math.round(z);
 
-    if (y >= 87  && y <= 92) {
+    if (y >= 87 && y <= 92) {
       console.log("uspela si");
       setTimeout(() => {
         cameraSensor.width = cameraView.videoWidth;
@@ -25,34 +25,67 @@ const cameraView = document.querySelector("#camera--view"),
         cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
         cameraOutput.src = cameraSensor.toDataURL("image/webp");
         cameraOutput.classList.add("taken");
-      }, 5000);
-    } else { console.log("greska")}
+      }, 2000);
+    }
   },
   true
 );
-     
+if (y === 0) {
+  setTimeout(() => {
+    console.log("hello");
+  }, 2000);
+}
 
-    
+function cameraStart() {
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function(stream) {
+      track = stream.getTracks()[0];
+      cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+      console.error("Oops. Something is broken.", error);
+    });
+}
+// Take a picture when cameraTrigger is tapped
 
-    
-        function cameraStart() {
-            navigator.mediaDevices
-                .getUserMedia(constraints)
-                .then(function(stream) {
-                track = stream.getTracks()[0];
-                cameraView.srcObject = stream;
-            })
-            .catch(function(error) {
-                console.error("Oops. Something is broken.", error);
-            });
-        }
-        // Take a picture when cameraTrigger is tapped
-        
-        // Start the video stream when the window loads
-        window.addEventListener("load", cameraStart, false);
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
 
-     
-
-
-      
 // Access the device camera and stream to cameraView
+
+var ball = document.querySelector(".ball");
+var garden = document.querySelector(".garden");
+var output = document.querySelector(".output");
+
+var maxX = garden.clientWidth - ball.clientWidth;
+var maxY = garden.clientHeight - ball.clientHeight;
+
+function handleOrientation(event) {
+  var x = event.beta; // In degree in the range [-180,180]
+  var y = event.gamma; // In degree in the range [-90,90]
+
+  output.innerHTML = "beta : " + x + "\n";
+  output.innerHTML += "gamma: " + y + "\n";
+
+  // Because we don't want to have the device upside down
+  // We constrain the x value to the range [-90,90]
+  if (x > 90) {
+    x = 90;
+  }
+  if (x < -90) {
+    x = -90;
+  }
+
+  // To make computation easier we shift the range of
+  // x and y to [0,180]
+  x += 90;
+  y += 90;
+
+  // 10 is half the size of the ball
+  // It center the positioning point to the center of the ball
+  ball.style.top = (maxX * x) / 180 - 10 + "px";
+  ball.style.left = (maxY * y) / 180 - 10 + "px";
+}
+
+window.addEventListener("deviceorientation", handleOrientation);
