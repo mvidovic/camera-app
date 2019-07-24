1c76abfa75f055c3ad4be8bf3257e3d9e765eb2b
Milica Vidovic <milica.vidovic92@gmail.com>
2019-07-24 09:25:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ window.addEventListener(
 
     if (y >= 87 && y <= 92) {
       setTimeout(() => {
-        ball.style.top = (maxY * y) / 180 - 10 + "px";
-        ball.style.left = (maxX * x) / 180 - 10 + "px";
+        ball.style.top = (maxX * x) / 180 - 10 + "px";
+        ball.style.left = (maxY * y) / 180 - 10 + "px";
         cameraSensor.width = cameraView.videoWidth;
         cameraSensor.height = cameraView.videoHeight;
         cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
