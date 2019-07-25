790799d8efeadb9c3fdeca46747d87f74330b665
Milica Vidovic <milica.vidovic92@gmail.com>
2019-07-25 17:21:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ function handleOrientation(event) {
   var y = event.beta;  // In degree in the range [-180,180]
   var x = event.gamma; // In degree in the range [-90,90]
 
-  output.innerHTML  = "beta : " + x + "\n";
-  output.innerHTML += "gamma: " + y + "\n";
+  output.innerHTML  = "beta : " + y + "\n";
+  output.innerHTML += "gamma: " + x + "\n";
 
   // Because we don't want to have the device upside down
   // We constrain the x value to the range [-90,90]
@@ -27,13 +27,13 @@ function handleOrientation(event) {
 
   // To make computation easier we shift the range of 
   // x and y to [0,180]
-  x += 90;
-  y += 90;
+//   x += 90;
+//   y += 90;
 
   // 10 is half the size of the ball
   // It center the positioning point to the center of the ball
-  ball.style.top  = (maxX*x/180 - 10) + "px";
-  ball.style.left = (maxY*y/180 - 10) + "px";
+  ball.style.left  = (maxX*x/180 - 10) + "px";
+  ball.style.top = (maxY*y/180 - 10) + "px";
 }
 
 window.addEventListener('deviceorientation', handleOrientation);
