5b464052280c2a246ad70da19cf3370174c3a582
Milica Vidovic <milica.vidovic92@gmail.com>
2019-07-25 17:25:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ function handleOrientation(event) {
 
   // 10 is half the size of the ball
   // It center the positioning point to the center of the ball
-  ball.style.left  = (maxX*x/180 - 10) + "px";
-  ball.style.top = (maxY*y/180 - 10) + "px";
+  ball.style.top  = (maxX*x/180 - 10) + "px";
+  ball.style.left = (maxY*y/180 - 10) + "px";
 }
 
 window.addEventListener('deviceorientation', handleOrientation);
