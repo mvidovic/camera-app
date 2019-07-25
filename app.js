6d6a36f233326138fc3cb19442cf2881cb2622e3
Milica Vidovic <milica.vidovic92@gmail.com>
2019-07-25 19:45:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ function handleOrientation(event) {
 
   // Because we don't want to have the device upside down
   // We constrain the x value to the range [-90,90]
-//   if (x >  90) { x =  90};
-//   if (x < -90) { x = -90};
+  if (x >  90) { x =  90};
+  if (x < -90) { x = -90};
 
   // To make computation easier we shift the range of 
   // x and y to [0,180]
