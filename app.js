8027298bd867b143f83614027ca33761deefc47c
Milica Vidovic <milica.vidovic92@gmail.com>
2019-07-25 20:07:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,53 +6,56 @@ const cameraView = document.querySelector("#camera--view"),
   cameraSensor = document.querySelector("#camera--sensor"),
   cameraTrigger = document.querySelector("#camera--trigger");
 
-var ball   = document.querySelector('.ball');
-var garden = document.querySelector('.garden');
-var output = document.querySelector('.output');
+var ball = document.querySelector(".ball");
+var garden = document.querySelector(".garden");
+var output = document.querySelector(".output");
 
-var maxX = garden.clientWidth  - ball.clientWidth;
+var maxX = garden.clientWidth - ball.clientWidth;
 var maxY = garden.clientHeight - ball.clientHeight;
 
 function handleOrientation(event) {
-  var y = event.beta;  // In degree in the range [-180,180]
+  var y = event.beta; // In degree in the range [-180,180]
   var x = event.gamma; // In degree in the range [-90,90]
 
-    y = Math.round(y);
-   x = Math.round(x);
-  
-  output.innerHTML  = "beta : " + y + "\n";
+  y = Math.round(y);
+  x = Math.round(x);
+
+  output.innerHTML = "beta : " + y + "\n";
   output.innerHTML += "gamma: " + x + "\n";
 
   // Because we don't want to have the device upside down
   // We constrain the x value to the range [-90,90]
-  if (x >  90) { x =  90};
-  if (x < -90) { x = -90};
+  if (x > 90) {
+    x = 90;
+  }
+  if (x < -90) {
+    x = -90;
+  }
 
-  // To make computation easier we shift the range of 
+  // To make computation easier we shift the range of
   // x and y to [0,180]
   x += 90;
   y += 90;
 
   // 10 is half the size of the ball
   // It center the positioning point to the center of the ball
-  ball.style.left  = (maxX*x/180 - 20) + "px";
-  ball.style.top = ((maxY*y/180) / 2 - 20) + "px";
-  console.log(y, 'yd');
-  console.log(x, 'x');
-    if (y >= 87 && y <= 92) {
-      console.log("uspela si");
-      setTimeout(() => {
-        cameraSensor.width = cameraView.videoWidth;
-        cameraSensor.height = cameraView.videoHeight;
-        cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-        cameraOutput.src = cameraSensor.toDataURL("image/webp");
-        cameraOutput.classList.add("taken");
-      }, 2000);
-    }
+  ball.style.left = (maxX * x) / 180 / 2 - 20 + "px";
+  ball.style.top = (maxY * y) / 180 / 2 - 20 + "px";
+  console.log(y, "yd");
+  console.log(x, "x");
+  if (y >= 87 && y <= 92) {
+    console.log("uspela si");
+    setTimeout(() => {
+      cameraSensor.width = cameraView.videoWidth;
+      cameraSensor.height = cameraView.videoHeight;
+      cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+      cameraOutput.src = cameraSensor.toDataURL("image/webp");
+      cameraOutput.classList.add("taken");
+    }, 2000);
   }
+}
 
-
-window.addEventListener('deviceorientation', handleOrientation);
+window.addEventListener("deviceorientation", handleOrientation);
 
 // window.addEventListener(
 //   "deviceorientation",
@@ -95,6 +98,3 @@ function cameraStart() {
 window.addEventListener("load", cameraStart, false);
 
 // // Access the device camera and stream to cameraView
-
-
-
